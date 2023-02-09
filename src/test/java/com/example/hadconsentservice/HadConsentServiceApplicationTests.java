1dc96935f563f8539a9834aefe7a04b2247e174a
rohitraman <rohitraman9944@gmail.com>
2023-02-09 23:27:16 +0530

    first commit

---
+++ src/test/java/com/example/hadconsentservice/HadConsentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hadconsentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadConsentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
