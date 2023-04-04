e91806b4976b4bda76eabd397615da03396d8963
Rahul Raj Ambastha <rahul2331998@gmail.com>
2023-04-04 21:40:29 +0530

    data folder added

---
+++ Data_Folder/cms_login.sql
@@ -0,0 +1,5 @@
+insert into login(id,name,password,role) VALUES
+(1,"Rahul","abcd","Doctor"),
+(2,"Rohit","abc","Doctor"),
+(3,"Prithvi","efgh","Patient"),
+(4,"Prathvi","efg","Patient");
--- cms_login.sql
+++
@@ -1,5 +0,0 @@
-insert into login(id,password,role) VALUES
-(1,"abcd","Doctor"),
-(2,"abc","Doctor"),
-(3,"efgh","Patient"),
-(4,"efg","Patient");
