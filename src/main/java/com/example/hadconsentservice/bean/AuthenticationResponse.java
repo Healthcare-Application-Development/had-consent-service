30ce787b15f3ee8ca26dbd373ae42086a861ee43
rohitraman <rohitraman9944@gmail.com>
2023-04-05 19:32:06 +0530

    Changed to JWT

---
+++ src/main/java/com/example/hadconsentservice/bean/AuthenticationResponse.java
@@ -0,0 +1,41 @@
+package com.example.hadconsentservice.bean;
+
+public class AuthenticationResponse {
+
+    private String accessToken;
+    private String name;
+    private Long id;
+    private String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
