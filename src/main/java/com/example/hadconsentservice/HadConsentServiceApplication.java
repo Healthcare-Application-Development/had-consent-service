f3e5454fa988a3c0ec51ccb0b6daf7bf54bc6597
rohitraman <rohitraman9944@gmail.com>
2023-04-24 00:25:17 +0530

    Removed comments

--- src/main/java/com/example/hadconsentservice/HadConsentServiceApplication.java
+++ src/main/java/com/example/hadconsentservice/HadConsentServiceApplication.java
@@ -1,23 +1,13 @@
 package com.example.hadconsentservice;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-import com.twilio.Twilio;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class HadConsentServiceApplication {
-	// @Value("${TWILIO_ACCOUNT_SID}")
-	// static String accountSid;
-
-	// @Value("${TWILIO_AUTH_KEY}")
-	// static String authKey;
-
 	public static void main(String[] args) {
-		// System.out.println(accountSid);
-		// Twilio.init(accountSid, authKey);
 		SpringApplication.run(HadConsentServiceApplication.class, args);
 	}
 
